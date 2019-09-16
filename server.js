91139fb13390b59c3b2f92459f1193e015788298
anaghakomawar <madhavir1982@gmail.com>
2019-09-16 15:10:59 +0530

    Anagha report

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const app 					= require('./app'); // app file include
 const globalVariable		= require('./nodemon');
 const port = process.env.PORT || globalVariable.PORT;
 const server = http.createServer(app);
-
+console.log("port ",port);
 server.listen(port);
 
