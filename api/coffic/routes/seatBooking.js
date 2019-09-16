a8bd639d916d5f324dd52fe537852fb820cd2cd7
Omkar R <omkarronghe99.or@gmail.com>
2019-09-16 12:10:00 +0530

    Omkar change in checkout

--- api/coffic/routes/seatBooking.js
+++ api/coffic/routes/seatBooking.js
@@ -15,7 +15,7 @@ router.get('/get/one/:user_id',seatBookingController.list_userSeatBooking);
 
 router.get('/get/availableSeats/:workspace_id',seatBookingController.availableSeats);
 
-router.patch('/patch/checkout', seatBookingController.checkoutUser);
+router.patch('/patch/chechout', seatBookingController.checkoutUser);
 // router.patch('/patch/checkout', seatBookingController.list_seatBooking);
 
 router.get('/validate/:user_id',seatBookingController.validate_checkin);
