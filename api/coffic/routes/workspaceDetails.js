2d73129092673c1503acdf01d79c040758dbc151
Omkar R <omkarronghe99.or@gmail.com>
2019-10-01 18:42:12 +0530

    Omkar change in notification

--- api/coffic/routes/workspaceDetails.js
+++ api/coffic/routes/workspaceDetails.js
@@ -12,7 +12,7 @@ router.get('/get/single/:user_id', workspaceController.id_cafeAdmin);
 
 router.post('/cafesearch', workspaceController.cafe_search);
 
-router.get('/get/list', workspaceController.list_workspace);
+router.get('/get/list/:center', workspaceController.list_workspace);
 
 router.get('/get/list/city', workspaceController.listcity_workspace);
 
