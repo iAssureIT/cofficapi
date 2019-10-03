107adb1c58ef902810884c8b705010b6004b3250
Omkar R <omkarronghe99.or@gmail.com>
2019-10-03 15:24:09 +0530

    Omkar change in workspace

--- api/coffic/controllers/workspaceDetails.js
+++ api/coffic/controllers/workspaceDetails.js
@@ -206,43 +206,43 @@ exports.list_workspace = (req,res,next)=>{
             for(k = 0 ; k < data.length ; k++){
             var inRange = await distance(data[k].location.latitude,data[k].location.longitude,req.body.lastlat,req.body.lastlong,"K")
             // var inRange = await getlatlongradious(data[k].location,req.body.lastlat,req.body.lastlong,20)
-            console.log('inRNGE=====>',inRange)
-            // if(inRange){
-            //     var seatData = await availableSeats(data[k]._id);
-            //     returndata.push({
-            //       "workspace_id"    : data[k]._id,
-            //       "nameOfCafe"      : data[k].nameOfCafe,
-            //       // "address"         : data[k].address,
-            //       // "landmark"        : data[k].landmark,
-            //       // "area"            : data[k].area,
-            //       // "city"            : data[k].city,
-            //       // "state"           : data[k].state,
-            //       // "country"         : data[k].country,
-            //       // "pin"             : data[k].pin,
-            //       "location"        : data[k].location,
-            //       "numberOfSeats"   : data[k].numberOfSeats,
-            //       // "name"            : data[k].name,
-            //       // "email"           : data[k].email,
-            //       // "facilities"      : data[k].facilities,
-            //       // "cost"            : data[k].cost,
-            //       "openingtime"     : data[k].openingtime,
-            //       "closingtime"     : data[k].closingtime,
-            //       "logo"            : data[k].logo,
-            //       "banner"          : data[k].banner,
-            //       "workspaceImages" : data[k].workspaceImages,
-            //       "seatData"        : seatData,
-            //       // "cafeAdmin"       : data[k].cafeAdmin,
-            //       // "bankDetails"     : data[k].bankDetails,
-            //     })  
-            //     if(k >= data.length){
-            //         res.status(200).json(returndata);
-            //      }
-            // }else{
-            //     res.status(200).json({
-            //         message : "No Work Space found in your area"
-            //     });
+            console.log('inRNGE=====>',parseInt(inRange))
+            if(parseInt(inRange)<= 20){
+                var seatData = await availableSeats(data[k]._id);
+                returndata.push({
+                  "workspace_id"    : data[k]._id,
+                  "nameOfCafe"      : data[k].nameOfCafe,
+                  // "address"         : data[k].address,
+                  // "landmark"        : data[k].landmark,
+                  // "area"            : data[k].area,
+                  // "city"            : data[k].city,
+                  // "state"           : data[k].state,
+                  // "country"         : data[k].country,
+                  // "pin"             : data[k].pin,
+                  "location"        : data[k].location,
+                  "numberOfSeats"   : data[k].numberOfSeats,
+                  // "name"            : data[k].name,
+                  // "email"           : data[k].email,
+                  // "facilities"      : data[k].facilities,
+                  // "cost"            : data[k].cost,
+                  "openingtime"     : data[k].openingtime,
+                  "closingtime"     : data[k].closingtime,
+                  "logo"            : data[k].logo,
+                  "banner"          : data[k].banner,
+                  "workspaceImages" : data[k].workspaceImages,
+                  "seatData"        : seatData,
+                  // "cafeAdmin"       : data[k].cafeAdmin,
+                  // "bankDetails"     : data[k].bankDetails,
+                })  
+                if(k >= data.length){
+                    res.status(200).json(returndata);
+                 }
+            }else{
+                res.status(200).json({
+                    message : "No Work Space found in your area"
+                });
 
-            // }
+            }
              }
              
         }
