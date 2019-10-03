4252e239fc403001005ffebfd335d3b9247d01ef
Omkar R <omkarronghe99.or@gmail.com>
2019-10-03 15:19:09 +0530

    Omkar change in workspace

--- api/coffic/controllers/workspaceDetails.js
+++ api/coffic/controllers/workspaceDetails.js
@@ -168,6 +168,27 @@ function getlatlongradious(location,centerlat,centerlong,range){
     return Math.sqrt(dx * dx + dy * dy) <= range
     
 }
+
+function distance(lat1, lon1, lat2, lon2, unit) {
+	if ((lat1 == lat2) && (lon1 == lon2)) {
+		return 0;
+	}
+	else {
+		var radlat1 = Math.PI * lat1/180;
+		var radlat2 = Math.PI * lat2/180;
+		var theta = lon1-lon2;
+		var radtheta = Math.PI * theta/180;
+		var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+		if (dist > 1) {
+			dist = 1;
+		}
+		dist = Math.acos(dist);
+		dist = dist * 180/Math.PI;
+		dist = dist * 60 * 1.1515;
+		if (unit=="K") { dist = dist * 1.609344 }
+		return dist;
+	}
+}
 exports.list_workspace = (req,res,next)=>{
     WorkspaceDetails
     .find()
@@ -183,44 +204,45 @@ exports.list_workspace = (req,res,next)=>{
             async function getData(){
             var returndata= [];
             for(k = 0 ; k < data.length ; k++){
-            var inRange = await getlatlongradious(data[k].location,req.body.lastlat,req.body.lastlong,20)
+            var inRange = await distance(data[k].location.latitude,data[k].location.longitude,req.body.lastlat,req.body.lastlong,"K")
+            // var inRange = await getlatlongradious(data[k].location,req.body.lastlat,req.body.lastlong,20)
             console.log('inRNGE=====>',inRange)
-            if(inRange){
-                var seatData = await availableSeats(data[k]._id);
-                returndata.push({
-                  "workspace_id"    : data[k]._id,
-                  "nameOfCafe"      : data[k].nameOfCafe,
-                  // "address"         : data[k].address,
-                  // "landmark"        : data[k].landmark,
-                  // "area"            : data[k].area,
-                  // "city"            : data[k].city,
-                  // "state"           : data[k].state,
-                  // "country"         : data[k].country,
-                  // "pin"             : data[k].pin,
-                  "location"        : data[k].location,
-                  "numberOfSeats"   : data[k].numberOfSeats,
-                  // "name"            : data[k].name,
-                  // "email"           : data[k].email,
-                  // "facilities"      : data[k].facilities,
-                  // "cost"            : data[k].cost,
-                  "openingtime"     : data[k].openingtime,
-                  "closingtime"     : data[k].closingtime,
-                  "logo"            : data[k].logo,
-                  "banner"          : data[k].banner,
-                  "workspaceImages" : data[k].workspaceImages,
-                  "seatData"        : seatData,
-                  // "cafeAdmin"       : data[k].cafeAdmin,
-                  // "bankDetails"     : data[k].bankDetails,
-                })  
-                if(k >= data.length){
-                    res.status(200).json(returndata);
-                 }
-            }else{
-                res.status(200).json({
-                    message : "No Work Space found in your area"
-                });
+            // if(inRange){
+            //     var seatData = await availableSeats(data[k]._id);
+            //     returndata.push({
+            //       "workspace_id"    : data[k]._id,
+            //       "nameOfCafe"      : data[k].nameOfCafe,
+            //       // "address"         : data[k].address,
+            //       // "landmark"        : data[k].landmark,
+            //       // "area"            : data[k].area,
+            //       // "city"            : data[k].city,
+            //       // "state"           : data[k].state,
+            //       // "country"         : data[k].country,
+            //       // "pin"             : data[k].pin,
+            //       "location"        : data[k].location,
+            //       "numberOfSeats"   : data[k].numberOfSeats,
+            //       // "name"            : data[k].name,
+            //       // "email"           : data[k].email,
+            //       // "facilities"      : data[k].facilities,
+            //       // "cost"            : data[k].cost,
+            //       "openingtime"     : data[k].openingtime,
+            //       "closingtime"     : data[k].closingtime,
+            //       "logo"            : data[k].logo,
+            //       "banner"          : data[k].banner,
+            //       "workspaceImages" : data[k].workspaceImages,
+            //       "seatData"        : seatData,
+            //       // "cafeAdmin"       : data[k].cafeAdmin,
+            //       // "bankDetails"     : data[k].bankDetails,
+            //     })  
+            //     if(k >= data.length){
+            //         res.status(200).json(returndata);
+            //      }
+            // }else{
+            //     res.status(200).json({
+            //         message : "No Work Space found in your area"
+            //     });
 
-            }
+            // }
              }
              
         }
